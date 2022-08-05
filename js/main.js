b400fd354ce04baec9ee29adc2946e0d44a1e149
Hernan Cersosimo <hscersosimo@gmail.com>
2022-08-05 16:26:28 -0300

    Modal - now with flexbox

--- js/main.js
+++ js/main.js
@@ -1,15 +1,18 @@
-// Get the modal
-var modal = document.getElementById("myModal");
+
+// Modal
+// ===========================
+
+// Get the modal elements
+var modal = document.getElementById("portfolioModal");
+var span = document.getElementsByClassName("close")[0];
+
 
 // Get the button that opens the modal
 var btn = document.getElementById("myBtn");
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
 // When the user clicks on the button, open the modal
 btn.onclick = function() {
-  modal.style.display = "block";
+  modal.style.display = "flex";
 }
 
 // When the user clicks on <span> (x), close the modal
