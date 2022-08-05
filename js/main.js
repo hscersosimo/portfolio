ca86440517266ebe039a584610eb474154f0cc7b
Hernan Cersosimo <hscersosimo@gmail.com>
2022-08-05 19:58:57 -0300

    Nav hover effect

--- js/main.js
+++ js/main.js
@@ -8,12 +8,23 @@ var span = document.getElementsByClassName("close")[0];
 
 
 // Get the button that opens the modal
-var btn = document.getElementById("myBtn");
+//var btn = document.getElementById("image-grid");
 
-// When the user clicks on the button, open the modal
-btn.onclick = function() {
-  modal.style.display = "flex";
-}
+// buttons is a node list. It looks and acts much like an array.
+const buttons = document.querySelectorAll('#image-grid img');
+
+// we use the .forEach method to iterate through each button
+buttons.forEach((button) => {
+
+    // and for each one we add a 'click' listener
+    button.addEventListener('click', () => {
+      modal.style.display = "flex";
+    });
+    // when the user clicks on the button, open the modal
+    button.onclick = function() {
+      modal.style.display = "flex";
+    }
+});
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
